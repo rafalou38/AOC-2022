c3ac2406e0dfaccc561b73de3416b1646764855d
Rafael <rafael.martinezcalvo@orange.fr>
2022-12-02 20:01:43 +0100

    02 part 2

--- 02/part-2.py
+++ 02/part-2.py
@@ -1,53 +1,56 @@
-with open("./input.txt", "r") as f:
-	rounds = [ln.split(" ") for ln in f.read().split("\n") if ln]
+O_ROCK = "A"
+O_PAPER = "B"
+O_SCISSORS = "C"
+
+P_ROCK = "A"
+P_PAPER = "B"
+P_SCISSORS = "C"
+
+LOOSE = "X"
+DRAW = "Y"
+WIN = "Z"
+
+with open("./02/input.txt", "r") as f:
+    rounds = [ln.split(" ") for ln in f.read().split("\n") if ln]
 
 
 def chooseAction(round: [int, int]):
-	actions = {"A": ["B", "A", "C"], "C": ["A", "C", "B"], "B": ["C", "B", "A"]}
+    actions = {
+        O_ROCK: [P_SCISSORS, P_ROCK, P_PAPER],
+        O_SCISSORS: [P_PAPER, P_SCISSORS, P_ROCK],
+        O_PAPER: [P_ROCK, P_PAPER, P_SCISSORS],
+    }
 
-	if (round[1] == "X"): return [-1, actions[round[0]][0]]
-	elif (round[1] == "Y"): return [0, actions[round[0]][1]]
-	elif (round[1] == "Z"): return [1, actions[round[0]][2]]
-
-
-def win(round: [int, int]):
-	"""
-	-1 : loose
-	 0 : draw
-	 1 : win
-	"""
-	if (round[0] == "A"):
-		if round[1] == "C": return -1
-		elif round[1] == "B": return 1
-	elif (round[0] == "B"):
-		if round[1] == "A": return -1
-		elif round[1] == "C": return 1
-	elif (round[0] == "C"):
-		if round[1] == "B": return -1
-		elif round[1] == "A": return 1
-
-	return 0
+    enemyMove = round[0]
+    outcome = round[1]
+    if outcome == WIN:
+        return [1, actions[enemyMove][2]]
+    elif outcome == DRAW:
+        return [0, actions[enemyMove][1]]
+    elif outcome == LOOSE:
+        return [-1, actions[enemyMove][0]]
 
 
 def getScore(win: bool, played: str):
-	score = 0
-	if (win == 0): score += 3
-	elif (win == 1): score += 6
+    score = 0
+    if win == 0:
+        score += 3
+    elif win == 1:
+        score += 6
 
-	score += {"A": 1, "B": 2, "C": 3}[played]
+    score += {P_ROCK: 1, P_PAPER: 2, P_SCISSORS: 3}[played]
 
-	return score
+    return score
 
 
 sum = 0
 for round in rounds:
-	action = chooseAction(round)
+    action = chooseAction(round)
 
-	result = action[0]
-	score = getScore(result, action[1])
+    score = getScore(action[0], action[1])
 
-	print(round, result, score)
+    # print(round, action)
 
-	sum += score
+    sum += score
 
 print("=>", sum)
